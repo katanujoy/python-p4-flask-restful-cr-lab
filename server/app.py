d530c40ba4233205fff2e9c87557185f19cfb9c6
katanujoy <katanujoyy99@gmail.com>
2025-07-02 08:15:56 +0300

    completed flask restful lab

--- server/app.py
+++ server/app.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Plant
-
+from models import db, Plant  
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
 
+db.init_app(app)  
 migrate = Migrate(app, db)
-db.init_app(app)
-
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return [plant.to_dict() for plant in plants], 200
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_plant = Plant(
+                name=data["name"],
+                image=data["image"],
+                price=data["price"]
+            )
+            db.session.add(new_plant)
+            db.session.commit()
+            return new_plant.to_dict(), 201
+        except Exception as e:
+            return {"error": str(e)}, 400
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if plant:
+            return plant.to_dict(), 200
+        else:
+            return {"error": "Plant not found"}, 404
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,25 +1,13 @@
-#!/usr/bin/env python3
-
 from app import app
 from models import db, Plant
 
 with app.app_context():
-
     Plant.query.delete()
 
-    aloe = Plant(
-        id=1,
-        name="Aloe",
-        image="./images/aloe.jpg",
-        price=11.50,
-    )
+    plants = [
+        Plant(name="Aloe", image="./images/aloe.jpg", price=11.50),
+        Plant(name="ZZ Plant", image="./images/zz-plant.jpg", price=25.98)
+    ]
 
-    zz_plant = Plant(
-        id=2,
-        name="ZZ Plant",
-        image="./images/zz-plant.jpg",
-        price=25.98,
-    )
-
-    db.session.add_all([aloe, zz_plant])
+    db.session.add_all(plants)
     db.session.commit()
